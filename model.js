9d157d9eae368c93ece613d46e7ce41518c6b3a6
muhamadabduh <muhamad.abduh.muh@gmail.com>
2018-09-12 13:18:20 +0700

    release 0

---
+++ model.js
@@ -0,0 +1,7 @@
+class Model{
+    static help(){
+        return 'Try this: todo help'
+    }
+}
+
+module.exports = Model
--- todo.js
+++ todo.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const Controller = require('./controller')
+
+let dataJson = fs.readFileSync('data.json')
+let data = JSON.parse(dataJson);
+
+// data.push()
+
+// console.log(data);
+
+// fs.writeFileSync(data2.json, finalData);
+if(process.argv[2] == undefined){
+    Controller.help();
+} else {
+    let command = process.argv[2]
+}
+
+let finalData = JSON.stringify(data, null, 2)
+
 
