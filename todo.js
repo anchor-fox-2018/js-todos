0813d622a1327d77102a6fab533a2c648067e539
Seneca Manu <dody@Seneca-Manu.local>
2018-09-16 18:25:58 +0700

    Release 1.1

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-let dataJsonIn = fs.readFileSync('data.json')
+let dataJsonIn = fs.readFileSync('data.json');
+let parsed = JSON.parse(dataJsonIn);
 
 class Task {
     static help() {
@@ -14,12 +15,59 @@ class Task {
         console.log('node todo.js complete <task id> - menandai kegiatan sebagai selesai');
         console.log('node todo.js incomplete <task id> - menandai kegiatan sebagai belum selesai');
     }
+
+    static list() {
+        for (let i = 0; i < parsed.length; i++) {
+            console.log(`${i + 1}: ${parsed[i].task}`);
+        }
+    }
+
+    static add(input) {
+        let stringed = input.toString()
+        let tempAdd = {
+            task: stringed
+        }
+
+        parsed.push(tempAdd);
+        let stringified = JSON.stringify(parsed);
+        fs.writeFileSync('data.json', stringified, 'utf8');
+    }
+
+    static findById(input) {
+        let numbered = Number(input);
+        console.log(`${1}: ${parsed[input - 1].task}`);
+    }
+
+    static delete(input) {
+        let numbered = Number(input);
+        console.log(`deleting '${parsed[numbered-1].task}' from todo list!`);
+        parsed.splice(numbered - 1, 1);
+
+        let stringified = JSON.stringify(parsed);
+        fs.writeFileSync('data.json', stringified, 'utf8');
+    }
 }
 
 // running classes through if's
 
 if (process.argv[2] === ('help' || undefined)) {
     Task.help();
+}
+
+else if (process.argv[2] === 'list') {
+    Task.list();
+}
+
+else if (process.argv[2] === 'add') {
+    Task.add(process.argv[3]);
+}
+
+else if (process.argv[2] === 'findById') {
+    Task.findById(process.argv[3]);
+}
+
+else if (process.argv[2] === 'delete') {
+    Task.delete(process.argv[3]);
 }
 
 else {
